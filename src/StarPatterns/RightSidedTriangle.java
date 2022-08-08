621e2201ec5a6c3cad0c0a3feefae4295628c056
eduqurd1989 <mashaedu@gmail.com>
2022-08-08 08:11:04 -0400

    E

---
+++ src/StarPatterns/RightSidedTriangle.java
@@ -0,0 +1,34 @@
+package StarPatterns;
+
+import java.util.Scanner;
+
+public class RightSidedTriangle {
+
+
+    private static  int  starstrianglePattern(int n) {
+        for (int i = 1; i <=n; i++){
+
+            for (int j = 1; j <= n- i;  j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        return n;
+
+    }
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Row Number");
+        int n = sc.nextInt();
+
+        starstrianglePattern(n);
+    }
+}
