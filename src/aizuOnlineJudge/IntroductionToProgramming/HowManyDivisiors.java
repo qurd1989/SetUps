9915836dedacbe1059b222d666c7c8dab32fd4f1
Qurd1989 <mashaedu@gmail.com>
2023-01-30 16:41:07 -0500

    Hi

---
+++ src/aizuOnlineJudge/IntroductionToProgramming/HowManyDivisiors.java
@@ -0,0 +1,26 @@
+package IntroductionToProgramming;
+
+import java.util.Scanner;
+
+public class HowManyDivisiors {
+
+    final static  Scanner sc = new Scanner(System.in);
+
+public static void main(String[] args) {
+
+
+
+    int a,b, c =10;
+
+    while (true) {
+
+        for (int i = 0;  i< c; i ++) {
+
+
+
+
+        }
+    }
+
+    }
+}
--- src/aizuOnlineJudge/SwappingTwoNumbers.java
+++ src/aizuOnlineJudge/SwappingTwoNumbers.java
@@ -1,2 +1,37 @@
-public class SwappingTwoNumbers {
+import java.util.Scanner;
+
+public class SwappingTwoNumbers{
+
+    static final Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+
+        int temp,x,y;
+
+        while (true) {
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            if (x == 0 && y == 0) {
+                break;
+
+            } else {
+
+                if (x > y) ;
+                temp = x;
+                x = y;
+                y = temp;
+
+                System.out.println(x + " " + y);
+
+
+            }
+
+        }
+    }
 }
+
+
