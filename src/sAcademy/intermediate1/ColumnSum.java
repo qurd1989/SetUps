cf73c9a95160aca2cced6f4114625c1f8fc7411e
Qurd1989 <mashaedu@gmail.com>
2023-03-10 21:03:37 -0500

    Hi

---
+++ src/sAcademy/intermediate1/ColumnSum.java
@@ -0,0 +1,26 @@
+package intermediate1;
+
+public class ColumnSum {
+
+    public static void main(String[] args) {
+
+    }
+    public  static int[] solve(int [][] A) {
+
+        int n = A.length;
+        int m = A[0].length;
+        int sum[] = new int[n];
+
+
+
+        for (int i = 0; i< n; i++) {
+
+
+            for (int j= 0; j< m; j++)
+
+
+            sum[j] += A[i][j];
+        }
+        return sum;
+    }
+}
---
+++ src/sAcademy/intermediate1/ScalarMatrice.java
@@ -0,0 +1,35 @@
+package intermediate1;
+
+public class ScalarMatrice {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static int[][] solve(int[][] A, int B) {
+
+
+        int n = A.length;
+        int m  = A[0].length;
+
+        int scalarMul[][] = new int[n][m];
+
+
+        for(int i =0; i < n; i++) {
+
+            for(int j =0; j < m; j++) {
+
+                scalarMul[i][j] = A[i][j] * B;
+
+
+            }
+        }
+
+        return scalarMul;
+
+    }
+}
+
+}
