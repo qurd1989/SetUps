1b5a1e8d8a76bcd9a615d639eadd0bba350e2a23
Qurd1989 <mashaedu@gmail.com>
2023-04-10 06:23:58 -0400

    Challenges

---
+++ src/sAcademy/intermediat2/CountSubarrayZeroSum.java
@@ -0,0 +1,26 @@
+package intermediat2;
+
+import java.util.HashSet;
+
+public class CountSubarrayZeroSum {
+    public static void main(String[] args) {
+
+    }
+
+    public int solve(int[] A) {
+
+        int n = A.length;
+
+        HashSet<Long> hs = new HashSet<>();
+
+        long prefix = 0;
+
+        for (int i = 0; i < n; i++) {
+
+
+        }
+
+
+    }
+
+}
