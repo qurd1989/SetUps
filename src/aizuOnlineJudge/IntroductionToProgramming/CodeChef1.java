c25a12768b50742baee745f4dc545e133907651f
Qurd1989 <mashaedu@gmail.com>
2023-05-18 22:46:26 -0400

    Challenges

--- src/aizuOnlineJudge/IntroductionToProgramming/CodeChef1.java
+++ src/aizuOnlineJudge/IntroductionToProgramming/CodeChef1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CodeChef1 {
 
 
-    public static void main (String[] args) throws java.lang.Exception
+    public static void main (String[] args)
     {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
