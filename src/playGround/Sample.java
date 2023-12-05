9c92a5ebbb5520c24b3d164a693cbfa43e8abbf9
Qurd1989 <mashaedu@gmail.com>
2023-12-04 21:01:51 -0500

    Challenges

---
+++ src/playGround/Sample.java
@@ -0,0 +1,20 @@
+package playGround;
+
+import java.util.HashMap;
+
+public class Sample {
+    public static void main(String[] args) {
+        int a = 3;
+        int b =5;
+
+        int c = a | b;
+        System.out.println(c);
+        int x = a & b;
+        System.out.println(x);
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+
+
+    }
+}
