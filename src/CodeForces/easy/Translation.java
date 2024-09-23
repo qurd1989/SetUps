7172d25e166460457dbb61739a26afd401b543da
Qurd1989 <mashaedu@gmail.com>
2024-09-22 20:35:45 -0400

    Challenges

--- src/CodeForces/easy/Translation.java
+++ src/CodeForces/easy/Translation.java
@@ -22,6 +22,7 @@ public class Translation {
         for (int i = 0; i < n/2; i++) {
             if (s.charAt(i) != t.charAt(n - i - 1)) {
                 System.out.println(s.charAt(i) + " " + t.charAt(n - i - 1));
+                System.out.println(s.charAt(n - i - 1) + " " + t.charAt(i));
                 return false;
             }
         }
