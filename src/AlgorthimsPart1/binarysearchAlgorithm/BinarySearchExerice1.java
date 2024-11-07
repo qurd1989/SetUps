61d2ca38c506d5bb2811200aa65db50b83e85280
eduqurd1989 <mashaedu@gmail.com>
2024-11-06 20:47:11 -0500

    my new commit

--- src/AlgorthimsPart1/binarysearchAlgorithm/BinarySearchExerice1.java
+++ src/AlgorthimsPart1/binarysearchAlgorithm/BinarySearchExerice1.java
@@ -5,6 +5,10 @@ public class BinarySearchExerice1 {
     public static void main(String[] args) {
 
         int[] arr = {1,2,3,4,5,6,7,8,9,0,34,65,6,5,4,3,7,8};
+        System.out.println(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
 
     }
 }
