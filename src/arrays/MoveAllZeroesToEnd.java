5f1e8e68cf05e78201b308f34bb4bfd3bfc4e42d
Qurd1989 <mashaedu@gmail.com>
2024-11-22 21:14:05 -0500

    Challenges

--- src/arrays/MoveAllZeroesToEnd.java
+++ src/arrays/MoveAllZeroesToEnd.java
@@ -4,4 +4,16 @@ public class MoveAllZeroesToEnd {
     public static void main(String[] args) {
 
     }
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0){
+                arr[count++] = arr[i];
+            }
+        }
+        while(count < n){
+            arr[count++] = 0;
+        }
+    }
 }
