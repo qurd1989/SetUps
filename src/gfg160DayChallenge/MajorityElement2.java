8b121748a3f01caa25ca6e5d9c0046ccc35df2e6
Qurd1989 <mashaedu@gmail.com>
2024-12-01 14:28:12 -0500

    Challenges

--- src/gfg160DayChallenge/MajorityElement2.java
+++ src/gfg160DayChallenge/MajorityElement2.java
@@ -1,5 +1,13 @@
 package gfg160DayChallenge;
 
+import java.util.List;
+
 public class MajorityElement2 {
 
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> findMajority(int[] nums){
+
+    }
 }
