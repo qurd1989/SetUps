3bc3321f05da8032f87908acd61ac20dd6f053be
Qurd1989 <mashaedu@gmail.com>
2024-12-03 23:23:41 -0500

    Challenges

--- src/gfg160DayChallenge/MajorityElement2.java
+++ src/gfg160DayChallenge/MajorityElement2.java
@@ -1,5 +1,6 @@
 package gfg160DayChallenge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MajorityElement2 {
@@ -8,6 +9,42 @@ public class MajorityElement2 {
 
     }
     public List<Integer> findMajority(int[] nums){
-
+        int candidate1 = 0;
+        int candidate2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int num : nums){
+            if (candidate1 == num){
+                count1++;
+            }else if (candidate2 == num){
+                count2++;
+            }else if (count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            }else if (count2 == 0){
+                candidate2 = num;
+                count2 = 1;
+            }else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums){
+            if (num == candidate1){
+                count1++;
+            }else if (num == candidate2){
+                count2++;
+            }
+        }
+        List<Integer> majorityElements = new ArrayList<>();
+        if (count1 > nums.length / 3){
+            majorityElements.add(candidate1);
+        }
+        if (count2 > nums.length / 3){
+            majorityElements.add(candidate2);
+        }
+        return majorityElements;
     }
 }
