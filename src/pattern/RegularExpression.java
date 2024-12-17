e748decbfb5b52da24d4219468bddabd1e6f9d78
Qurd1989 <mashaedu@gmail.com>
2024-12-16 23:29:23 -0500

    Challenges

--- src/pattern/RegularExpression.java
+++ src/pattern/RegularExpression.java
@@ -1,4 +1,13 @@
 package pattern;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegularExpression {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("a*b");
+        Matcher m = p.matcher("aaaaab");
+        boolean b = m.matches();
+        System.out.println(b);
+    }
 }
