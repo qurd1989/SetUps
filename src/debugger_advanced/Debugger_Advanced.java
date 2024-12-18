e05dcb81436ada3738f93981a84c8697cab95f76
Qurd1989 <mashaedu@gmail.com>
2024-12-17 22:58:57 -0500

    Challenges

--- src/debugger_advanced/Debugger_Advanced.java
+++ src/debugger_advanced/Debugger_Advanced.java
@@ -1,4 +1,26 @@
 package debugger_advanced;
 
-public class Debugger_Advanced {
+
+
+public  class Debugger_Advanced {
+    static Debugger_Advanced instatnce;
+    public static void main(String[] args) {
+        System.out.println("Debugger Advanced");
+    }
+    public static class Cache{
+        static Cache instance;
+        public static void main(String[] args) {
+            System.out.println("Debugger Advanced");
+            for(int i = 0; i < 10; i++){
+                System.out.println(getInstance(i));
+            }
+        }
+
+        private static Cache getInstance(int i) {
+            if (instance == null){
+                instance = new Cache();
+            }
+            return instance;
+        }
+    }
 }
