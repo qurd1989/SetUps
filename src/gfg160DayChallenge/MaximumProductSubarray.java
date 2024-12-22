fb683201b315f10124f33f55971cdfbccc9e1ac0
Qurd1989 <mashaedu@gmail.com>
2024-12-21 23:17:27 -0500

    Challenges

--- src/gfg160DayChallenge/MaximumProductSubarray.java
+++ src/gfg160DayChallenge/MaximumProductSubarray.java
@@ -5,6 +5,18 @@ public class MaximumProductSubarray {
 
     }
     int maxProduct(int[] arr) {
-
+        int n = arr.length;
+        int max = arr[0];
+        int min = arr[0];
+        int result = arr[0];
+        for (int i = 1; i < n; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * arr[i], min * arr[i]), arr[i]);
+            min = Math.min(Math.min(temp * arr[i], min * arr[i]), arr[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
     }
 }
