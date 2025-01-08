0a1d664c9dae0e168c5bc9b2dfc87de6f8463272
Qurd1989 <mashaedu@gmail.com>
2025-01-07 22:52:46 -0500

    Challenges

---
+++ src/learnYard/fundamentals/ArrayTypes.java
@@ -0,0 +1,39 @@
+package fundamentals;
+
+public class ArrayTypes {
+    public static void main(String[] args) {
+
+    }
+    void arrayTypes() {
+        // 1. Array of primitive data types
+        int[] arr = new int[5];
+        // 2. Array of objects
+        String[] str = new String[5];
+        // 3. Array of arrays
+        int[][] arr2 = new int[5][5];
+        // 4. Array of objects
+        String[][] str2 = new String[5][5];
+        // 5. Array of objects
+        String[][][] str3 = new String[5][5][5];
+        // 6. Array of objects
+        String[][][][] str4 = new String[5][5][5][5];
+        // 7. Array of objects
+        String[][][][][] str5 = new String[5][5][5][5][5];
+        // 8. Array of objects
+        String[][][][][][] str6 = new String[5][5][5][5][5][5];
+        // 9. Array of objects
+        String[][][][][][][] str7 = new String[5][5][5][5][5][5][5];
+        // 10. Array of objects
+        String[][][][][][][][] str8 = new String[5][5][5][5][5][5][5][5];
+        // 11. Array of objects
+        String[][][][][][][][][] str9 = new String[5][5][5][5][5][5][5][5][5];
+        // 12. Array of objects
+        String[][][][][][][][][][] str10 = new String[5][5][5][5][5][5][5][5][5][5];
+        // 13. Array of objects
+        String[][][][][][][][][][][] str11 = new String[5][5][5][5][5][5][5][5][5][5][5];
+        // 14. Array of objects
+        String[][][][][][][][][][][][] str12 = new String[5][5][5][5][5][5][5][5][5][5][5][5];
+        // 15. Array of objects
+        String[][][][][][][][][][][][][] str13 = new String[5][5][5][5][5][5][5][5][5][5][5][5][5];
+    }
+}
