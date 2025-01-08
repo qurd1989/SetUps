da08bedc6304f58021a37ef754dd19453e55b84b
Qurd1989 <mashaedu@gmail.com>
2025-01-07 22:58:11 -0500

    Challenges

--- src/learnYard/fundamentals/ArrayTypes.java
+++ src/learnYard/fundamentals/ArrayTypes.java
@@ -2,9 +2,11 @@ package fundamentals;
 
 public class ArrayTypes {
     public static void main(String[] args) {
+        ArrayTypes obj = new ArrayTypes();
+        System.out.println(obj.arrayTypes());
 
     }
-    void arrayTypes() {
+    String arrayTypes() {
         // 1. Array of primitive data types
         int[] arr = new int[5];
         // 2. Array of objects
@@ -21,19 +23,7 @@ public class ArrayTypes {
         String[][][][][] str5 = new String[5][5][5][5][5];
         // 8. Array of objects
         String[][][][][][] str6 = new String[5][5][5][5][5][5];
-        // 9. Array of objects
-        String[][][][][][][] str7 = new String[5][5][5][5][5][5][5];
-        // 10. Array of objects
-        String[][][][][][][][] str8 = new String[5][5][5][5][5][5][5][5];
-        // 11. Array of objects
-        String[][][][][][][][][] str9 = new String[5][5][5][5][5][5][5][5][5];
-        // 12. Array of objects
-        String[][][][][][][][][][] str10 = new String[5][5][5][5][5][5][5][5][5][5];
-        // 13. Array of objects
-        String[][][][][][][][][][][] str11 = new String[5][5][5][5][5][5][5][5][5][5][5];
-        // 14. Array of objects
-        String[][][][][][][][][][][][] str12 = new String[5][5][5][5][5][5][5][5][5][5][5][5];
-        // 15. Array of objects
-        String[][][][][][][][][][][][][] str13 = new String[5][5][5][5][5][5][5][5][5][5][5][5][5];
+
+        return "Array types initialized";
     }
 }
