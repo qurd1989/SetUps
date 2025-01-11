23715e9f466603fbdf3cdba716cb7e05b8fbbf28
Qurd1989 <mashaedu@gmail.com>
2025-01-10 23:14:57 -0500

    Challenges

--- src/gfg160DayChallenge/ArrayReverse.java
+++ src/gfg160DayChallenge/ArrayReverse.java
@@ -1,7 +1,22 @@
 package gfg160DayChallenge;
 
+import java.util.*;
+
 public class ArrayReverse {
     public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 2, 8, 5, 6, 8, 6};
+        System.out.println("Second largest element: " + findSecondLargest(arr));
+    }
 
+    public static int findSecondLargest(int[] arr) {
+        Map<Integer, Boolean> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, true);
+        }
+
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys, Collections.reverseOrder());
+
+        return keys.size() > 1 ? keys.get(1) : -1;
     }
 }
