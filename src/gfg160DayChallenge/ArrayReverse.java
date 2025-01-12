6671a6946b34b03c76316acaeb00c882bfc9b73a
Qurd1989 <mashaedu@gmail.com>
2025-01-11 22:25:04 -0500

    Challenges

--- src/gfg160DayChallenge/ArrayReverse.java
+++ src/gfg160DayChallenge/ArrayReverse.java
@@ -30,4 +30,12 @@ public class ArrayReverse {
         }
 
     }
+    static void reversArray1(int []arr) {
+        int n = arr.length;
+        for (int i = 0; i < n / 2; i++){
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+}
 }
