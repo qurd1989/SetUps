6a4d28f5b6a51cddf3d12f10eb9528bd61c09243
Qurd1989 <mashaedu@gmail.com>
2025-02-11 00:52:01 -0500

    Challenges

--- src/CodeForces/easy/SKibidusAndOhio.java
+++ src/CodeForces/easy/SKibidusAndOhio.java
@@ -6,25 +6,24 @@ public class SKibidusAndOhio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        sc.nextLine();  // Consume newline after reading t
-
         while (t-- > 0) {
-            String s = sc.nextLine();
-            int minLength = countUniqueCharacterGroups(s);
-            System.out.println(minLength);
-        }
-        sc.close();
-    }
+            String s = sc.next();
+            int n = s.length();
+            boolean flag = false;
 
-    private static int countUniqueCharacterGroups(String s) {
-        int minLength = 0;  // Count unique character groups
+            for (int i = 1; i < n; i++) {
+                if (s.charAt(i - 1) == s.charAt(i)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
 
-        for (int i = 0; i < s.length(); i++) {
-            // Count only the first occurrence of a new group
-            if (i == 0 || s.charAt(i) != s.charAt(i - 1)) {
-                minLength++;
+                System.out.println(1);
+
+            } else {
+                System.out.println(n);
             }
         }
-        return minLength;
     }
 }
