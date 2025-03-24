8e4ae34015be751604e594417851392500ae3ec9
Qurd1989 <mashaedu@gmail.com>
2025-03-24 09:45:11 -0400

    sfdfds

--- src/pattern/RightAngleTriangleNumberPattern.java
+++ src/pattern/RightAngleTriangleNumberPattern.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 
 public class RightAngleTriangleNumberPattern {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
 
-        int raws = sc.nextInt();
-
-        for (int i = 1; i <= raws; i++){
-
-            for (int j = 1; j <= i; j++){
-                System.out.print(j + "  ");
-            }
-            System.out.println();
-            System.out.println();
+        int arr[] = {1, 2, 0, 4, 0, 5, 0, 7};
+        int result[] = printRightAngleTriangleNumberPattern(arr);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
         }
     }
+
+    public static int[] printRightAngleTriangleNumberPattern(int arr[] ){
+        int n = arr.length;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+            while (n > count){
+                arr[count++] = 0;
+
+            }
+            return arr;
+        }
 
 }
