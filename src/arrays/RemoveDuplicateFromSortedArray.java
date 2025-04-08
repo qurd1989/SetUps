54a80c2e1b5b2bd57c207cbfc1ddae3d47c681a4
Qurd1989 <mashaedu@gmail.com>
2025-04-07 23:49:41 -0400

    Challenges

--- src/arrays/RemoveDuplicateFromSortedArray.java
+++ src/arrays/RemoveDuplicateFromSortedArray.java
@@ -1,5 +1,20 @@
 package arrays;
 
-public class RemoveDuplicateFromSortedArray {
+import java.util.Enumeration;
 
+public class RemoveDuplicateFromSortedArray {
+    public static void main(String[] args) {
+
+    }
+    public static int[] removeDup(int arr[]) {
+        int n = arr.length;
+        int num = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i-1] != arr[i]){
+                arr[num] = arr[i];
+                num++;
+            }
+        }
+        return arr;
+    }
 }
