908658ec128d0049d54626c6b1fafb659b0f9639
Qurd1989 <mashaedu@gmail.com>
2025-04-08 23:46:12 -0400

    Applied Merge Sort Array

--- src/algorithms/divideAndConquer/MergeSort.java
+++ src/algorithms/divideAndConquer/MergeSort.java
@@ -1,14 +1,53 @@
 package divideAndConquer;
 
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
 
     }
 
 
-    public static int merge(int arr[], int left, int mid, int right, int tempArray[]) {
+    public static void merge(int arr[], int low, int mid, int high) {
 
-        return 0;
+        int num[] = new int[arr.length];
+        int left = low;
+        int right = mid +1;
+        while ((left <= mid && right <= high)) {
+            if (arr[left] <= arr[right]) {
+                num[left] = arr[left];
+                left++;
+            } else {
+                num[right] = arr[right];
+                right++;
+            }
+        }
+        while (left <= mid) {
+            num[left] = arr[left];
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            num[right] = arr[right];
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = num[i-low];
+        }
+    }
+
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
     }
 
 }
