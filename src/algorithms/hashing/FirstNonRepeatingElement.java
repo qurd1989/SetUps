57c0b03971f1a74ba73c6ae3ffd0f4aa8b9518d8
Qurd1989 <mashaedu@gmail.com>
2025-04-17 19:42:08 -0400

    First Non-Repeating character

---
+++ src/algorithms/hashing/FirstNonRepeatingElement.java
@@ -0,0 +1,27 @@
+package hashing;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeatingElement {
+    public static void main(String[] args) {
+
+    }
+    public  static char firstNonRepeatingElement(String s) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()){
+            if (entry.getKey() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+}
